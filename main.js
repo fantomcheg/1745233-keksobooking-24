81600df7c8cfec5fe7721315d5e0957a5acbea8b
Biksileev, Dmitry <dmitry.biksileev@x5.ru>
2021-10-01 13:03:53 +0500

    add main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+function randomInteger(min, max) {
+  if(min>=0 && max>=0){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {console.log('Введите числа больше 0')}
+}
+
+console.log(randomInteger(-1,3));
+
