ef25af2f6cd54590f7da27d980ec1875801a35fe
Biksileev, Dmitry <dmitry.biksileev@x5.ru>
2021-10-02 16:42:48 +0500

    добавил Throw NE

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ function getRandomInteger(min, max) {
   if(min>=0 && max>=0){
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }  else if (min >= max){
-    console.log('Минимальное число не может быть больше или равно максимальному')
+    throw new Error('Минимальное число не может быть больше или равно максимальному')
   } else {
-    console.log('Введите числа больше 0')}
+    throw new Error('Введите числа больше 0')}
 }
 
 console.log(getRandomInteger(2,3));
