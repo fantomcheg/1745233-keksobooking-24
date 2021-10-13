26174f19a017b6beb2f78bb3a0847e797b117bc4
Biksileev, Dmitry <dmitry.biksileev@x5.ru>
2021-10-13 11:04:27 +0500

    Add second function

--- main.js
+++ main.js
@@ -7,4 +7,15 @@ function getRandomInteger(min, max) {
     throw new Error('Введите числа больше 0')}
 }
 
-console.log(getRandomInteger(2,3));
+console.log(getRandomInteger(2,10));
+
+function getRandomFloat(min, max,num) {
+  if(min>=0 && max>=0){
+    return ((Math.random() * (max - min + 1)) + min).toFixed(num);
+  }  else if (min >= max){
+    throw new Error('Минимальное число не может быть больше или равно максимальному')
+  } else {
+    throw new Error('Введите числа больше 0')}
+}
+
+console.log(getRandomFloat(2,10,4));
